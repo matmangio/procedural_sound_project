1ae4b4fba012518b255e8485c980baf57b59f7fa
Matteo Mangioni <mat.mangioni@gmail.com>
2026-08-24 17:16:48 +0200

    Init project

---
+++ SoundProject/Source/SoundProject/SoundProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SoundProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SoundProject, "SoundProject" );
