4669995bf365e083848cf60131bf4af20d80c390
Matteo Mangioni <mat.mangioni@gmail.com>
2026-08-29 20:22:30 +0200

    Update temp OneZeroFilter with proper c++ filter

---
+++ SoundProject/Plugins/MetasoundExtras/Source/MetasoundExtras/Private/Nodes/OneZeroFilterNode.cpp
@@ -0,0 +1,107 @@
+﻿#include "Nodes/OneZeroFilterNode.h"
+
+#define LOCTEXT_NAMESPACE "MetasoundExtras_CosNode"
+
+namespace MetasoundExtras {
+	
+	using namespace Metasound;
+
+	namespace OneZeroFilter {
+		// Input params
+		METASOUND_PARAM(In, "In", "The audio input buffer.");
+		METASOUND_PARAM(Coefficient, "Coefficient", "The filter coefficient.");
+		
+		// Output params
+		METASOUND_PARAM(Out, "Out", "The audio output buffer.");
+	}
+
+	// Constructor
+	FOneZeroFilterOperator::FOneZeroFilterOperator(const FOperatorSettings& InSettings, const FAudioBufferReadRef& InAudio, const FFloatReadRef& InCoefficient)
+		: In(InAudio), Coefficient(InCoefficient), Out(FAudioBufferWriteRef::CreateNew(InSettings)) {}
+
+	const FNodeClassMetadata& FOneZeroFilterOperator::GetNodeInfo() {
+		auto InitNodeInfo = []() -> FNodeClassMetadata
+		{
+			FNodeClassMetadata Info;
+
+			Info.ClassName         = { TEXT("UE"), TEXT("OneZeroFilter"), TEXT("Audio") };
+			Info.MajorVersion      = 1;
+			Info.MinorVersion      = 0;
+			Info.DisplayName       = LOCTEXT("MetasoundExtras_OneZeroDisplayName", "OneZeroFilter");
+			Info.Description       = LOCTEXT("MetasoundExtras_OneZeroNodeDescription", "Raw one-zero non-recursive real filter.");
+			Info.Author            = PluginAuthor;
+			Info.PromptIfMissing   = PluginNodeMissingPrompt;
+			Info.DefaultInterface  = GetVertexInterface();
+			Info.CategoryHierarchy = { LOCTEXT("MetasoundExtras_OneZeroNodeCategory", "Extras") };
+
+			return Info;
+		};
+
+		static const FNodeClassMetadata Info = InitNodeInfo();
+
+		return Info;
+	}
+
+	void FOneZeroFilterOperator::BindInputs(FInputVertexInterfaceData& InOutVertexData) {
+		using namespace OneZeroFilter;
+		
+		InOutVertexData.BindReadVertex(METASOUND_GET_PARAM_NAME(In), In);
+		InOutVertexData.BindReadVertex(METASOUND_GET_PARAM_NAME(Coefficient), Coefficient);
+	}
+
+	void FOneZeroFilterOperator::BindOutputs(FOutputVertexInterfaceData& InOutVertexData) {
+		using namespace OneZeroFilter;
+
+		InOutVertexData.BindWriteVertex(METASOUND_GET_PARAM_NAME(Out), Out);
+	}
+
+	const FVertexInterface& FOneZeroFilterOperator::GetVertexInterface() {
+		using namespace OneZeroFilter;
+
+		static const FVertexInterface Interface(
+			FInputVertexInterface(
+				TInputDataVertex<FAudioBuffer>(METASOUND_GET_PARAM_NAME_AND_METADATA(In)),
+				TInputDataVertex<float>(METASOUND_GET_PARAM_NAME_AND_METADATA(Coefficient), 1.0f)
+			),
+
+			FOutputVertexInterface(
+				TOutputDataVertex<FAudioBuffer>(METASOUND_GET_PARAM_NAME_AND_METADATA(Out))
+			)
+		);
+
+		return Interface;
+	}
+
+	TUniquePtr<IOperator> FOneZeroFilterOperator::CreateOperator(const FBuildOperatorParams& InParams, FBuildResults& OutResults) {
+		using namespace OneZeroFilter;
+		
+		FAudioBufferReadRef InRef = InParams.InputData.GetOrCreateDefaultDataReadReference<FAudioBuffer>(
+			METASOUND_GET_PARAM_NAME(In), InParams.OperatorSettings);
+		FFloatReadRef CoefficientRef = InParams.InputData.GetOrCreateDefaultDataReadReference<float>(
+			METASOUND_GET_PARAM_NAME(Coefficient), InParams.OperatorSettings);
+
+		return MakeUnique<FOneZeroFilterOperator>(InParams.OperatorSettings, InRef, CoefficientRef);
+	}
+
+	void FOneZeroFilterOperator::Execute() {
+		const float* InputAudio = In->GetData();
+		float* OutputAudio = Out->GetData();
+		
+		const int32 NumSamples = In->Num();
+		
+		for (int32 i = 0; i < NumSamples; i++) {
+			OutputAudio[i] = InputAudio[i];
+			if (i > 0) {
+				OutputAudio[i] -= *Coefficient * InputAudio[i-1];
+			}
+		}
+	}
+
+	void FOneZeroFilterOperator::Reset(const IOperator::FResetParams& InParams) {
+		Out->Zero();
+	}
+
+	METASOUND_REGISTER_NODE(FZeroOneFilterNode)
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ SoundProject/Plugins/MetasoundExtras/Source/MetasoundExtras/Public/Nodes/OneZeroFilterNode.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "HAL/Platform.h"
+#include "MetasoundEnumRegistrationMacro.h"
+#include "MetasoundParamHelper.h"
+
+namespace MetasoundExtras {
+	
+	// Computes cosine of a value in [0, 1]
+	class FOneZeroFilterOperator : public Metasound::TExecutableOperator<FOneZeroFilterOperator> {
+	public:
+		FOneZeroFilterOperator(const Metasound::FOperatorSettings& InSettings, const Metasound::FAudioBufferReadRef& InAudio, const Metasound::FFloatReadRef& InCoefficient);
+
+		static const Metasound::FNodeClassMetadata& GetNodeInfo();
+
+		virtual void BindInputs(Metasound::FInputVertexInterfaceData& InOutVertexData) override;
+		virtual void BindOutputs(Metasound::FOutputVertexInterfaceData& InOutVertexData) override;
+
+		static const Metasound::FVertexInterface& GetVertexInterface();
+		static TUniquePtr<Metasound::IOperator> CreateOperator(const Metasound::FBuildOperatorParams& InParams, Metasound::FBuildResults& OutResults);
+
+		void Execute();
+		
+		void Reset(const IOperator::FResetParams& InParams);
+
+	private:
+
+		// Inputs
+		Metasound::FAudioBufferReadRef In;
+		Metasound::FFloatReadRef Coefficient;
+		
+		// Outputs
+		Metasound::FAudioBufferWriteRef Out;
+		
+	};
+
+	using FZeroOneFilterNode = Metasound::TNodeFacade<FOneZeroFilterOperator>;
+}
